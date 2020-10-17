9dc0b0615e2c2e96561285ebe5a7b79e7b0ebd5c
Vain9 <Vain9@10.249.238.143>
2020-10-17 18:13:46 -0500

    added code to complete assignment

--- public_html/LoopsAssignment1.js
+++ public_html/LoopsAssignment1.js
@@ -3,5 +3,13 @@
  * 0, 10, 20, 30, ... 1000
  */
 
-document.write("This program displays the numbers 0 to 1000 by 10's");
 
+var counter = 1000;
+
+for (var i = 10; i <= counter; i=i+10) {
+
+document.write("<br>" + i);
+
+}
+
+document.write("This program displays the numbers 0 to 1000 by 10's");
